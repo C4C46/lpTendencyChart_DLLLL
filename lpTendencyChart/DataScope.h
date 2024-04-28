7e96145decbc69f101ad955c1e5a8f2b00b26cfe
cao.peixuan <cao.peixuan@hzleaper.com>
2024-04-28 13:10:00 +0800

    1

---
+++ lpTendencyChart/DataScope.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include <QObject>
+#include <QTableWidget>
+#include <QList>
+#include <QHeaderView>
+#include <QEvent>
+#include <QScrollBar>
+
+class DataScope : public QObject {
+	Q_OBJECT
+
+public:
+	DataScope(QTableWidget* tableWidget, QObject* parent = nullptr);
+	void setColumnNames(const QStringList &names);
+	void addData(const QString & curveName, double x, double y, const QVariantList & warningValue, const QVariantList & alarmValue);
+
+protected:
+	bool eventFilter(QObject *obj, QEvent *event);
+
+private:
+	QTableWidget* data_tableWidget;
+	QStringList m_columnNames;
+	bool autoScrollEnabled = true;  // 默认启用自动滚动
+};
+
+
